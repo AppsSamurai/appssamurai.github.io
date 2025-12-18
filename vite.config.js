6a6e9114de46e7fcc1c54b8e279f1b4115f9fe80
rasika <rasika.sugathadasa@e-jan.co.jp>
2025-12-18 18:04:48 +0900

    fix: Update script asset path in index.html.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/AppsSamurai/appssamurai.github.io/'
+    base: '/appssamurai.github.io/'
 })
